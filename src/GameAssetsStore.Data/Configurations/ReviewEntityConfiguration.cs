f7c31c49571ad2221837c9c64b353e141cfc0e27
Ivan Kukutsov <ivankukucov@gmail.com>
2023-07-02 13:10:08 +0300

    Added configuration for Review entity.

---
+++ src/GameAssetsStore.Data/Configurations/ReviewEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace GameAssetsStore.Data.Configurations;
+
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using GameAssetsStore.Data.Models;
+
+public class ReviewEntityConfiguration : IEntityTypeConfiguration<Review>
+{
+    public void Configure(EntityTypeBuilder<Review> builder)
+    {
+        builder
+            .HasOne(e => e.ReviewedAsset)
+            .WithMany(e => e.Reviews)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
