5ee345c7a7478ad189ceb5a95e8c988015d2f4ce
Ivan Kukutsov <ivankukucov@gmail.com>
2023-07-03 14:52:58 +0300

    Repository interface added.

---
+++ src/GameAssetsStore.Data/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace GameAssetsStore.Data.Repositories.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    IQueryable<TEntity> GetAll();
+
+    IQueryable<TEntity> GetAllAsNoTracking();
+
+    Task AddAsync(TEntity entity);
+
+    Task UpdateAsync(TEntity entity);
+
+    void Delete(TEntity entity);
+
+    Task<int> SaveChangesAsync();
+}
