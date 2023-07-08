61e1bdf16b779ad439d6b44e09fc51ade9c2f884
Ivan Kukutsov <ivankukucov@gmail.com>
2023-07-08 22:16:12 +0300

    Seller to shop entity rename migration added.

--- src/GameAssetsStore.Data/Migrations/20230708191405_SellerNameChangeToShop.Designer.cs
+++ src/GameAssetsStore.Data/Migrations/20230708191405_SellerNameChangeToShop.Designer.cs
@@ -4,6 +4,7 @@ using GameAssetsStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GameAssetsStore.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230708191405_SellerNameChangeToShop")]
+    partial class SellerNameChangeToShop
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/GameAssetsStore.Data/Migrations/20230708191405_SellerNameChangeToShop.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameAssetsStore.Data.Migrations
+{
+    public partial class SellerNameChangeToShop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assets_Sellers_SellerId",
+                table: "Assets");
+
+            migrationBuilder.DropTable(
+                name: "Sellers");
+
+            migrationBuilder.RenameColumn(
+                name: "SellerId",
+                table: "Assets",
+                newName: "ShopId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Assets_SellerId",
+                table: "Assets",
+                newName: "IX_Assets_ShopId");
+
+            migrationBuilder.CreateTable(
+                name: "Shops",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OwningUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false, comment: "User owning the shop profile."),
+                    ShopName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true, comment: "Optional name for the seller page."),
+                    SupportEmail = table.Column<string>(type: "nvarchar(max)", nullable: true, comment: "Optional email address for asset questions and support.")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shops", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Shops_AspNetUsers_OwningUserId",
+                        column: x => x.OwningUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shops_OwningUserId",
+                table: "Shops",
+                column: "OwningUserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assets_Shops_ShopId",
+                table: "Assets",
+                column: "ShopId",
+                principalTable: "Shops",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assets_Shops_ShopId",
+                table: "Assets");
+
+            migrationBuilder.DropTable(
+                name: "Shops");
+
+            migrationBuilder.RenameColumn(
+                name: "ShopId",
+                table: "Assets",
+                newName: "SellerId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Assets_ShopId",
+                table: "Assets",
+                newName: "IX_Assets_SellerId");
+
+            migrationBuilder.CreateTable(
+                name: "Sellers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OwningUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false, comment: "User owning the seller profile."),
+                    SellerName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true, comment: "Optional name for the seller page."),
+                    SupportEmail = table.Column<string>(type: "nvarchar(max)", nullable: true, comment: "Optional email address for asset questions and support.")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sellers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sellers_AspNetUsers_OwningUserId",
+                        column: x => x.OwningUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sellers_OwningUserId",
+                table: "Sellers",
+                column: "OwningUserId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assets_Sellers_SellerId",
+                table: "Assets",
+                column: "SellerId",
+                principalTable: "Sellers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
