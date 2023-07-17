339a6de3a2f62e35664a0234fc77df4bf7664ff1
Ivan Kukutsov <ivankukucov@gmail.com>
2023-07-17 22:17:41 +0300

    Added shop area

---
+++ src/GameAssetsStore.Web/Areas/Shop/Controllers/ManageController.cs
@@ -0,0 +1,14 @@
+﻿namespace GameAssetsStore.Web.Areas.Shop.Controllers;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Area("Shop")]
+[Authorize(Policy = "ShopOwner")]
+public class ManageController : Controller
+{
+    public IActionResult Assets()
+    {
+        return View();
+    }
+}
