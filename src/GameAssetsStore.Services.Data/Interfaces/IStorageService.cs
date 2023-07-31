157c4c3d24f55f672b557eeadd15fede54ad3a5a
Ivan Kukutsov <ivankukucov@gmail.com>
2023-07-31 23:30:33 +0300

    Added IStorageService interface to replace IObjectStore interface in future.

---
+++ src/GameAssetsStore.Services.Data/Interfaces/IStorageService.cs
@@ -0,0 +1,8 @@
+﻿namespace GameAssetsStore.Services.Data.Interfaces;
+
+using Microsoft.AspNetCore.Http;
+
+public interface IStorageService
+{
+    Task UploadAsync(IFormFile file, string assetId, string container, string encodedFilename);
+}
