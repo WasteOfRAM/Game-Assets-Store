26f55f012b05914b63dffbb13f3f95f8d3964374
Ivan Kukutsov <ivankukucov@gmail.com>
2023-08-02 13:52:49 +0300

    Create AssetController.cs

---
+++ src/GameAssetsStore.Web/Controllers/AssetController.cs
@@ -0,0 +1,39 @@
+﻿namespace GameAssetsStore.Web.Controllers;
+
+using GameAssetsStore.Services.Data.Interfaces;
+using GameAssetsStore.Web.Infrastructure.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize]
+public class AssetController : Controller
+{
+    private readonly IAssetService assetService;
+
+    public AssetController(IAssetService assetService)
+    {
+        this.assetService = assetService;
+    }
+
+    public async Task<IActionResult> Download(string assetId)
+    {
+        try
+        {
+            if (await this.assetService.IsUserAssetOwnerAsync(User.GetShopId(), assetId) ||
+                await this.assetService.IsUserPurchasedAssetAsync(User.GetId()!, assetId))
+            {
+                var file = await assetService.DownloadAsync(assetId);
+
+                return File(file.FileStream, file.ContentType, file.FileName);
+            }
+
+            return Unauthorized();
+        }
+        catch (Exception)
+        {
+            // TODO: Handle it properly
+
+            throw;
+        }
+    }
+}
