88dc2b4ee6082207cd3a708b86261fe9ce7cae33
Ivan Kukutsov <ivankukucov@gmail.com>
2023-08-02 13:51:08 +0300

    Created DownloadAssetServiceModel to transfer files from S3StorageService to AssetService and controller action.

---
+++ src/GameAssetsStore.Services.Models/Asset/DownloadAssetServiceModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GameAssetsStore.Services.Models.Asset;
+
+public class DownloadAssetServiceModel
+{
+    public byte[] FileStream { get; set; } = null!;
+
+    public string? FileName { get; set; }
+
+    public string ContentType { get; set; } = null!;
+}
