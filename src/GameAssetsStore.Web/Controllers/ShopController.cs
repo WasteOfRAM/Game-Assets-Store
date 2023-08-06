70858c1e57bf16071a3571587c1a364f6551c5fa
Ivan Kukutsov <ivankukucov@gmail.com>
2023-08-07 00:19:44 +0300

    ShopContoller that replaced the homecontroller

---
+++ src/GameAssetsStore.Web/Controllers/ShopController.cs
@@ -0,0 +1,66 @@
+﻿namespace GameAssetsStore.Web.Controllers;
+
+using GameAssetsStore.Services.Data.Interfaces;
+using GameAssetsStore.Web.ViewModels;
+using GameAssetsStore.Web.ViewModels.Shop;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+[Authorize]
+public class ShopController : Controller
+{
+    private readonly IAssetService assetService;
+    private readonly IShopService shopService;
+
+    public ShopController(
+        IAssetService assetService,
+        IShopService shopService)
+    {
+        this.assetService = assetService;
+        this.shopService = shopService;
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var model = await this.shopService.GetHomePageAssetsAsync();
+
+        return View(model);
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IActionResult> Browse([FromQuery] AssetQueryModel queryModel)
+    {
+        var model = await this.shopService.GetAllAssetsAsync(queryModel);
+
+
+        return View(model);
+    }
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IActionResult> Asset(string id) 
+    {
+        try
+        {
+            var model = await this.assetService.GetAssetPageViewModelAsync(id);
+
+            return View(model);
+        }
+        catch (Exception)
+        {
+            // TODO: 
+
+            throw;
+        }
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
