7e560283941432487083f63c6922087c3e4c1567
Ivan Kukutsov <ivankukucov@gmail.com>
2023-08-07 00:26:52 +0300

    Added AssetCardViewModel and AssetCardPartial view

---
+++ src/GameAssetsStore.Web.ViewModels/Shop/AssetCardViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GameAssetsStore.Web.ViewModels.Shop;
+
+public class AssetCardViewModel
+{
+    public string Id { get; set; } = null!;
+
+    public string AssetName { get; set; } = null!;
+
+    public decimal? Price { get; set; }
+
+    public string ImageUrl { get; set; } = null!;
+}
