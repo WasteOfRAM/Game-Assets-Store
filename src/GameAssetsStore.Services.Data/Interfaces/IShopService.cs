ed359702f04596d8ddf97b7be4f74c20c5f2bf8d
Ivan Kukutsov <ivankukucov@gmail.com>
2023-08-07 00:35:29 +0300

    Added shop service

---
+++ src/GameAssetsStore.Services.Data/Interfaces/IShopService.cs
@@ -0,0 +1,10 @@
+﻿namespace GameAssetsStore.Services.Data.Interfaces;
+
+using GameAssetsStore.Web.ViewModels.Shop;
+
+public interface IShopService
+{
+    Task<BrowsePageViewModel> GetAllAssetsAsync(AssetQueryModel queryModel);
+
+    Task<ShopHomePageViewModel> GetHomePageAssetsAsync();
+}
---
+++ src/GameAssetsStore.Services.Data/ShopService.cs
@@ -0,0 +1,79 @@
+﻿namespace GameAssetsStore.Services.Data;
+
+using GameAssetsStore.Data.Models;
+using GameAssetsStore.Data.Repositories.Interfaces;
+using GameAssetsStore.Services.Data.Interfaces;
+using GameAssetsStore.Web.ViewModels.Shop;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+using static Common.GlobalConstants;
+
+public class ShopService : IShopService
+{
+    private readonly IRepository<Asset> assetRepository;
+
+    public ShopService(IRepository<Asset> assetRepository)
+    {
+        this.assetRepository = assetRepository;
+    }
+
+    public async Task<BrowsePageViewModel> GetAllAssetsAsync(AssetQueryModel queryModel)
+    {
+        var allAssetsQuery = this.assetRepository.GetAllAsNoTracking()
+            .Where(a => a.IsPublic);
+
+        BrowsePageViewModel viewModel = new BrowsePageViewModel();
+
+        if (!string.IsNullOrWhiteSpace(queryModel.Search))
+        {
+            allAssetsQuery = allAssetsQuery
+                .Where(a => a.AssetName.ToUpper().Contains(queryModel.Search.ToUpper()));
+        }
+
+        viewModel.FilteredAssets = await allAssetsQuery
+            .Select(a => new AssetCardViewModel
+            {
+                Id = a.Id.ToString().ToLower(),
+                AssetName = a.AssetName,
+                Price = a.Price,
+                ImageUrl = string.Format(AWSS3ImageUrl, AWSS3Region, a.Id.ToString().ToLower(), "cover")
+            })
+                .ToListAsync();
+
+        return viewModel;
+    }
+
+
+    public async Task<ShopHomePageViewModel> GetHomePageAssetsAsync()
+    {
+        ShopHomePageViewModel model = new ShopHomePageViewModel();
+
+        model.AssetsByUploadDate = await this.assetRepository.GetAllAsNoTracking()
+            .Where(a => a.IsPublic)
+            .OrderByDescending(a => a.CreatedOn)
+            .Take(IndexPageAssetCountPerCategory)
+            .Select(a => new AssetCardViewModel
+            {
+                Id = a.Id.ToString().ToLower(),
+                AssetName = a.AssetName,
+                Price = a.Price,
+                ImageUrl = string.Format(AWSS3ImageUrl, AWSS3Region, a.Id.ToString().ToLower(), "cover")
+            })
+            .ToListAsync();
+
+        model.FreeAssets = await this.assetRepository.GetAllAsNoTracking()
+            .Where(a => a.IsPublic && a.Price == null)
+            .Take(IndexPageAssetCountPerCategory)
+            .Select(a => new AssetCardViewModel
+            {
+                Id = a.Id.ToString().ToLower(),
+                AssetName = a.AssetName,
+                Price = a.Price,
+                ImageUrl = string.Format(AWSS3ImageUrl, AWSS3Region, a.Id.ToString().ToLower(), "cover")
+            })
+            .ToListAsync();
+
+        return model;
+    }
+}
