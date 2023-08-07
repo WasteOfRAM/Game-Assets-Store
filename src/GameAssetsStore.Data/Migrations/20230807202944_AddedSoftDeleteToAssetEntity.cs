3a3a17446f80f3b6fb5c8e26a1b1b943b52fc552
Ivan Kukutsov <ivankukucov@gmail.com>
2023-08-07 23:33:31 +0300

    Asset entity soft delete DB migration.

---
+++ src/GameAssetsStore.Data/Migrations/20230807202944_AddedSoftDeleteToAssetEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameAssetsStore.Data.Migrations
+{
+    public partial class AddedSoftDeleteToAssetEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                table: "Assets",
+                type: "datetime2",
+                nullable: true,
+                comment: "Date the asset was deleted");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Assets",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                comment: "Soft delete");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                table: "Assets");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Assets");
+        }
+    }
+}
