bf06fb9f8e0964e233f83e8018408361cd1f2cb4
Ivan Kukutsov <ivankukucov@gmail.com>
2023-08-13 03:38:43 +0300

    Tests

--- src/GameAssetStore.Services.Tests/AssetServicesTests.cs
+++ src/GameAssetStore.Services.Tests/AssetServicesTests.cs
@@ -9,6 +9,9 @@ using Microsoft.EntityFrameworkCore;
 using Moq;
 using NUnit.Framework;
 using System.Threading.Tasks;
+using GameAssetsStore.Web.ViewModels.Manage;
+
+using static Common.GlobalConstants;
 
 using static GameAssetsStore.Data.Seeding.AssetSeed;
 using static GameAssetsStore.Data.Seeding.ShopsSeed;
@@ -37,6 +40,7 @@ public class AssetServicesTests
             .Options;
 
         this.dbContext = new ApplicationDbContext(options);
+
         //this.dbContext.AddRange(GenerateAssets());
         //this.dbContext.AddRange(GenerateShops());
         //this.dbContext.AddRange(GenerateApplicationUsers());
@@ -357,5 +361,194 @@ public class AssetServicesTests
 
         // Assert
         Assert.IsFalse(result);
+    }
+
+    [Test]
+    public async Task GetShopManagerAssetViewModel_ReturnsListOfViewModel()
+    {
+        // Arrange
+        string shopId = "25C159F2-7159-4DA5-A5E1-8D0081C6E2E1".ToLower();
+
+        List<ManageAssetCardViewModel> expected = new List<ManageAssetCardViewModel>
+        {
+            new ManageAssetCardViewModel
+            {
+                Id = new Guid("C017A10C-80F3-4184-AC8B-2B912C15D568"),
+                AssetName = "Cylinder",
+                FileName = "Cylinder.zip",
+                Description = null,
+                CoverImageUrl = string.Format(AWSS3ImageUrl, AWSS3Region, "C017A10C-80F3-4184-AC8B-2B912C15D568".ToLower(), "cover"),
+                ArtStyle = "Futuristic",
+                Price = 2.00m,
+                Version = "A",
+                SalesCount = 0,
+                CreatedOn = new DateTime(2023, 08, 03, 20, 30, 1),
+                ModifiedOn = null,
+                IsPublic = false
+            }
+        };
+
+        var asset = new Asset
+        {
+            Id = new Guid("C017A10C-80F3-4184-AC8B-2B912C15D568"),
+            ShopId = new Guid("25C159F2-7159-4DA5-A5E1-8D0081C6E2E1"),
+            AssetName = "Cylinder",
+            Description = null,
+            Price = 2.00m,
+            Version = "A",
+            SalesCount = 0,
+            CreatedOn = new DateTime(2023, 08, 03, 20, 30, 1),
+            IsPublic = false,
+            FileName = "Cylinder.zip",
+            ArtStyleId = new Guid("B8EC1523-E249-45CC-8C48-EF78915A5E52"),
+            IsDeleted = false
+        };
+
+        var user = new ApplicationUser
+        {
+            Id = new Guid("37216E26-1916-41FB-B264-5D06F7872225"),
+            UserName = "user1",
+            NormalizedUserName = "USER1",
+            Email = "user1@test.bg",
+            NormalizedEmail = "USER1@TEST.BG",
+            EmailConfirmed = true,
+            PasswordHash = "AQAAAAEAACcQAAAAEEnNe39P9D9blooFfyu/rirNk7a8Cw5ggIVuNauhHCXSprf5phaf6XuYNgBZiUq7UA==",
+            SecurityStamp = "HNPD455OYW2GR6AGOIK7IWTPSCRJPVBX",
+            ConcurrencyStamp = "021fa647-4713-4e6e-9649-c9e33242d45a",
+            PhoneNumberConfirmed = true,
+            TwoFactorEnabled = false,
+            LockoutEnabled = false,
+            AccessFailedCount = 0,
+            PaymentMethodId = new Guid("11FC7D8E-9363-47AF-8D0C-E0EF73C471F7")
+        };
+
+        this.dbContext.Add(user);
+
+        this.dbContext.Add(new Shop
+        {
+            Id = new Guid("25C159F2-7159-4DA5-A5E1-8D0081C6E2E1"),
+            OwningUserId = new Guid("37216E26-1916-41FB-B264-5D06F7872225"),
+            ShopName = "Good Stuff",
+            ShopAssets = new List<Asset> {  asset }
+        });
+
+        this.dbContext.AddRange( asset);
+
+        var artStyle1 = new ArtStyle
+        {
+            Id = new Guid("B8EC1523-E249-45CC-8C48-EF78915A5E52"),
+            Name = "Futuristic"
+        };
+
+        var artStyle2 = new ArtStyle
+        {
+            Id = new Guid("E7592028-354C-46CE-ADEC-B41525CCF712"),
+            Name = "Sci-Fi"
+        };
+
+        this.dbContext.AddRange(artStyle1, artStyle2);
+
+        this.dbContext.SaveChanges();
+
+        var assetService = new AssetService(this.assetsRepository,
+            this.shopRepository,
+            this.generalCategoryRepository,
+            this.subCategoryRepository,
+            this.artStyleRepository,
+            this.applicationUserRepository,
+            this.mockStorageService.Object);
+
+        // Act
+
+        var actual = await assetService.GetShopManagerAssetViewModelAsync(shopId);
+
+        // Assert
+        Assert.That(expected[0].Id, Is.EqualTo(actual[0].Id));
+    }
+
+    [Test]
+    public async Task GetShopManagerAssetViewModel_ReturnsEmptyListIfShopHasNoAssets()
+    {
+        // Arrange
+        string shopId = "D83EDC2E-D407-4411-B750-E7E55FB28FC4".ToLower();
+
+        int expected = 0;
+
+        var asset = new Asset
+        {
+            Id = new Guid("C017A10C-80F3-4184-AC8B-2B912C15D568"),
+            ShopId = new Guid("25C159F2-7159-4DA5-A5E1-8D0081C6E2E1"),
+            AssetName = "Cylinder",
+            Description = null,
+            Price = 2.00m,
+            Version = "A",
+            SalesCount = 0,
+            CreatedOn = new DateTime(2023, 08, 03, 20, 30, 1),
+            IsPublic = false,
+            FileName = "Cylinder.zip",
+            ArtStyleId = new Guid("B8EC1523-E249-45CC-8C48-EF78915A5E52"),
+            IsDeleted = false
+        };
+
+        var user = new ApplicationUser
+        {
+            Id = new Guid("37216E26-1916-41FB-B264-5D06F7872225"),
+            UserName = "user1",
+            NormalizedUserName = "USER1",
+            Email = "user1@test.bg",
+            NormalizedEmail = "USER1@TEST.BG",
+            EmailConfirmed = true,
+            PasswordHash = "AQAAAAEAACcQAAAAEEnNe39P9D9blooFfyu/rirNk7a8Cw5ggIVuNauhHCXSprf5phaf6XuYNgBZiUq7UA==",
+            SecurityStamp = "HNPD455OYW2GR6AGOIK7IWTPSCRJPVBX",
+            ConcurrencyStamp = "021fa647-4713-4e6e-9649-c9e33242d45a",
+            PhoneNumberConfirmed = true,
+            TwoFactorEnabled = false,
+            LockoutEnabled = false,
+            AccessFailedCount = 0,
+            PaymentMethodId = new Guid("11FC7D8E-9363-47AF-8D0C-E0EF73C471F7")
+        };
+
+        this.dbContext.Add(user);
+
+        this.dbContext.Add(new Shop
+        {
+            Id = new Guid("25C159F2-7159-4DA5-A5E1-8D0081C6E2E1"),
+            OwningUserId = new Guid("37216E26-1916-41FB-B264-5D06F7872225"),
+            ShopName = "Good Stuff",
+            ShopAssets = new List<Asset> { asset }
+        });
+
+        this.dbContext.AddRange(asset);
+
+        var artStyle1 = new ArtStyle
+        {
+            Id = new Guid("B8EC1523-E249-45CC-8C48-EF78915A5E52"),
+            Name = "Futuristic"
+        };
+
+        var artStyle2 = new ArtStyle
+        {
+            Id = new Guid("E7592028-354C-46CE-ADEC-B41525CCF712"),
+            Name = "Sci-Fi"
+        };
+
+        this.dbContext.AddRange(artStyle1, artStyle2);
+
+        this.dbContext.SaveChanges();
+
+        var assetService = new AssetService(this.assetsRepository,
+            this.shopRepository,
+            this.generalCategoryRepository,
+            this.subCategoryRepository,
+            this.artStyleRepository,
+            this.applicationUserRepository,
+            this.mockStorageService.Object);
+
+        // Act
+
+        var actual = await assetService.GetShopManagerAssetViewModelAsync(shopId);
+
+        // Assert
+        Assert.That(expected, Is.EqualTo(actual.Count()));
     }
 }
---
+++ src/GameAssetStore.Services.Tests/CartServiceTests.cs
@@ -0,0 +1,204 @@
+﻿namespace GameAssetsStore.Services.Tests;
+
+using GameAssetsStore.Data.Models;
+using GameAssetsStore.Data.Repositories.Interfaces;
+using GameAssetsStore.Data.Repositories;
+using GameAssetsStore.Services.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using GameAssetsStore.Web.ViewModels.Shop;
+using GameAssetsStore.Services.Data;
+using System.Xml.Linq;
+using System.Text.Json;
+
+public class CartServiceTests
+{
+    private IRepository<Asset> assetsRepository;
+    private IRepository<ApplicationUser> applicationUserRepository;
+
+    private ApplicationDbContext dbContext;
+
+    [SetUp]
+    public void TestSetup()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "InMemoryDb")
+            .Options;
+
+        this.dbContext = new ApplicationDbContext(options);
+
+        this.assetsRepository = new EfRepository<Asset>(this.dbContext);
+        this.applicationUserRepository = new EfRepository<ApplicationUser>(this.dbContext);
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        this.dbContext.Database.EnsureDeleted();
+    }
+
+    [Test]
+    public async Task GetCheckoutCartAssets_WithValidCartAndAssetsInIt()
+    {
+        // Arrange
+
+        string userId = "37216E26-1916-41FB-B264-5D06F7872225".ToLower();
+
+        string? cartJson = JsonSerializer.Serialize(new[] { new { AssetId = "97501e8c-cc13-4123-b79d-b4ef776919aa", Title = "Gear", Price = "33.33" } });
+
+        var expected = new CheckoutViewModel
+        {
+            CheckoutAssets = new[] { new CheckoutAssetViewModel { AssetId = "97501e8c-cc13-4123-b79d-b4ef776919aa", Title = "Gear" } },
+            PriceTotal = 33.33m,
+            PaymentMethodId = "11FC7D8E-9363-47AF-8D0C-E0EF73C471F7".ToLower(),
+            PaymentMethodName = "Bank1"
+        };
+
+        this.dbContext.Add(new Asset
+        {
+            Id = new Guid("97501E8C-CC13-4123-B79D-B4EF776919AA"),
+            ShopId = new Guid("25C159F2-7159-4DA5-A5E1-8D0081C6E2E1"),
+            AssetName = "Gear",
+            Description = "Cogwheel with a hole.",
+            Price = 33.33m,
+            Version = "43.5V",
+            SalesCount = 0,
+            CreatedOn = new DateTime(2023, 08, 03, 20, 31, 57),
+            IsPublic = true,
+            FileName = "gear.zip",
+            ArtStyleId = new Guid("D5BC94DD-A06C-4744-BE19-4206A84AB96C")
+        });
+
+        this.dbContext.Add(new ApplicationUser
+        {
+            Id = new Guid("37216E26-1916-41FB-B264-5D06F7872225"),
+            UserName = "user1",
+            NormalizedUserName = "USER1",
+            Email = "user1@test.bg",
+            NormalizedEmail = "USER1@TEST.BG",
+            EmailConfirmed = true,
+            PasswordHash = "AQAAAAEAACcQAAAAEEnNe39P9D9blooFfyu/rirNk7a8Cw5ggIVuNauhHCXSprf5phaf6XuYNgBZiUq7UA==",
+            SecurityStamp = "HNPD455OYW2GR6AGOIK7IWTPSCRJPVBX",
+            ConcurrencyStamp = "021fa647-4713-4e6e-9649-c9e33242d45a",
+            PhoneNumberConfirmed = true,
+            TwoFactorEnabled = false,
+            LockoutEnabled = false,
+            AccessFailedCount = 0,
+            PaymentMethodId = new Guid("11FC7D8E-9363-47AF-8D0C-E0EF73C471F7")
+        });
+
+        this.dbContext.Add(new PaymentMethod
+        {
+            Id = new Guid("11FC7D8E-9363-47AF-8D0C-E0EF73C471F7"),
+            Name = "Bank1"
+        });
+
+        this.dbContext.SaveChanges();
+
+        var cartService = new CartService(this.assetsRepository, this.applicationUserRepository);
+
+        // Act
+
+        var actual = await cartService.GetCheckoutCartAssetsAsync(userId, cartJson);
+
+        // Assert
+
+        Assert.That(actual.PriceTotal, Is.EqualTo(expected.PriceTotal));
+        Assert.That(actual.PaymentMethodName, Is.EqualTo(expected.PaymentMethodName));
+        Assert.That(actual.PaymentMethodId, Is.EqualTo(expected.PaymentMethodId));
+        Assert.That(actual.CheckoutAssets.Count(), Is.EqualTo(expected.CheckoutAssets.Count()));
+    }
+
+    [Test]
+    public async Task GetCheckoutCartAssets_WithValidEmptyCart()
+    {
+        // Arange
+
+        string userId = "37216E26-1916-41FB-B264-5D06F7872225".ToLower();
+
+        string? cartJson = "[]";
+
+
+        var expected = new CheckoutViewModel
+        {
+            PriceTotal = 0.0m,
+            PaymentMethodId = "11FC7D8E-9363-47AF-8D0C-E0EF73C471F7".ToLower(),
+            PaymentMethodName = "Bank1"
+        };
+
+        this.dbContext.Add(new Asset
+        {
+            Id = new Guid("97501E8C-CC13-4123-B79D-B4EF776919AA"),
+            ShopId = new Guid("25C159F2-7159-4DA5-A5E1-8D0081C6E2E1"),
+            AssetName = "Gear",
+            Description = "Cogwheel with a hole.",
+            Price = 33.33m,
+            Version = "43.5V",
+            SalesCount = 0,
+            CreatedOn = new DateTime(2023, 08, 03, 20, 31, 57),
+            IsPublic = true,
+            FileName = "gear.zip",
+            ArtStyleId = new Guid("D5BC94DD-A06C-4744-BE19-4206A84AB96C")
+        });
+
+        this.dbContext.Add(new ApplicationUser
+        {
+            Id = new Guid("37216E26-1916-41FB-B264-5D06F7872225"),
+            UserName = "user1",
+            NormalizedUserName = "USER1",
+            Email = "user1@test.bg",
+            NormalizedEmail = "USER1@TEST.BG",
+            EmailConfirmed = true,
+            PasswordHash = "AQAAAAEAACcQAAAAEEnNe39P9D9blooFfyu/rirNk7a8Cw5ggIVuNauhHCXSprf5phaf6XuYNgBZiUq7UA==",
+            SecurityStamp = "HNPD455OYW2GR6AGOIK7IWTPSCRJPVBX",
+            ConcurrencyStamp = "021fa647-4713-4e6e-9649-c9e33242d45a",
+            PhoneNumberConfirmed = true,
+            TwoFactorEnabled = false,
+            LockoutEnabled = false,
+            AccessFailedCount = 0,
+            PaymentMethodId = new Guid("11FC7D8E-9363-47AF-8D0C-E0EF73C471F7")
+        });
+
+        this.dbContext.Add(new PaymentMethod
+        {
+            Id = new Guid("11FC7D8E-9363-47AF-8D0C-E0EF73C471F7"),
+            Name = "Bank1"
+        });
+
+        this.dbContext.SaveChanges();
+
+        var cartService = new CartService(this.assetsRepository, this.applicationUserRepository);
+
+        // Act
+
+        var actual = await cartService.GetCheckoutCartAssetsAsync(userId, cartJson);
+
+        // Assert
+
+        Assert.That(actual.PriceTotal, Is.EqualTo(expected.PriceTotal));
+        Assert.That(actual.PaymentMethodName, Is.EqualTo(expected.PaymentMethodName));
+        Assert.That(actual.PaymentMethodId, Is.EqualTo(expected.PaymentMethodId));
+        Assert.That(actual.CheckoutAssets.Count(), Is.EqualTo(expected.CheckoutAssets.Count()));
+    }
+
+    [Test]
+    public void GetCheckoutCartAssets_WithNullCartThrowsException()
+    {
+        // Arrange
+
+        string userId = "37216E26-1916-41FB-B264-5D06F7872225".ToLower();
+
+        string? cartJson = null;
+
+        var cartService = new CartService(this.assetsRepository, this.applicationUserRepository);
+
+        // Act
+
+        // Assert
+
+        Assert.ThrowsAsync<NullReferenceException>(async () =>
+        {
+            await cartService.GetCheckoutCartAssetsAsync(userId, cartJson);
+        }, "The cart was not retrieved.");
+    }
+}
