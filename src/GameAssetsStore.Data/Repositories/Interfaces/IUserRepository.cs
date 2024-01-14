250ca4f1c2c7d2172c074f6c0338b37e6af73bef
Ivan Kukutsov <ivankukucov@gmail.com>
2024-01-14 15:58:17 +0200

    Added user repository

---
+++ src/GameAssetsStore.Data/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace GameAssetsStore.Data.Repositories.Interfaces;
+
+using GameAssetsStore.Data.Models;
+
+public interface IUserRepository : IRepository<ApplicationUser>
+{
+    Task<ApplicationUser?> GetByEmail(string email);
+
+    Task<ApplicationUser?> GetByUserName(string userName);
+}
---
+++ src/GameAssetsStore.Data/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace GameAssetsStore.Data.Repositories;
+
+using Data.Models;
+using Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+public class UserRepository : EfRepositoryBase<ApplicationUser>, IUserRepository
+{
+    public UserRepository(ApplicationDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<ApplicationUser?> GetByEmail(string email) => await this.DbSet.FirstOrDefaultAsync(u =>  u.Email == email);
+
+    public async Task<ApplicationUser?> GetByUserName(string userName) => await this.DbSet.FirstOrDefaultAsync(u => u.UserName ==  userName);
+}
