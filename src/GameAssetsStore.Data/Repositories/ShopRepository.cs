9d80cdf52f28db4ef2f91d153883f43d5f22805e
Ivan Kukutsov <ivankukucov@gmail.com>
2024-01-15 15:12:52 +0200

    Added shop repository

---
+++ src/GameAssetsStore.Data/Repositories/ShopRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace GameAssetsStore.Data.Repositories;
+
+using GameAssetsStore.Data.Models;
+using GameAssetsStore.Data.Repositories.Interfaces;
+
+public class ShopRepository : EfRepositoryBase<Shop>, IShopRepository
+{
+    public ShopRepository(ApplicationDbContext dbContext) : base(dbContext)
+    {
+    }
+}
